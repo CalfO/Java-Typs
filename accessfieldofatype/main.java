2bfc2584ff58e5e03b8ef4e554b70dc46c4008a0
Sylvain MAullet (SMA) <sylvain.maullet@inovatel.com>
2017-08-31 10:57:08 +0200

    Clean class name and add comment

--- accessfieldofatype/main.java
+++ accessfieldofatype/main.java
@@ -4,25 +4,25 @@ public static void main(String[] args){
 }
 
 /**
+ * Java 8
  * Utility method to access all field of type Date of any complex object as deep as he can.
  * Just change first line by corresponding class.
  * And use @Test to run it easily
  */
-// @Test
 public void getAllDateFieldNameOgDevice() {
-	Field[] deviceFields = Device.class.getDeclaredFields();
-	Set<String> dateFieldName = getDateFieldName(deviceFields, "root", Date.class);
+	Field[] deviceFields = MyObject.class.getDeclaredFields();
+	List<String> dateFieldName = getDateFieldName(deviceFields, "root", Date.class);
 	dateFieldName.stream().forEach(dfn -> System.out.print("'"+dfn+"',"));
 }
 
-private Set<String> getDateFieldName(Field[] deviceFields, String fatherField, Class<?> classToFind) {
-	Set<String> dateFieldName = new HashSet<>();
+private List<String> getDateFieldName(Field[] deviceFields, String fatherField, Class<?> classToFind) {
+	List<String> dateFieldName = new ArrayList<>();
 	for (Field field : deviceFields) {
 		Field[] subField = field.getType().getDeclaredFields();
-		if (subField != null && Arrays.stream(subField).anyMatch(sb -> sb.getType().equals(classToFind))) {
+		if (subField != null && Arrays.stream(subField).anyMatch(sb -> Objects.equals(sb.getType() ,classToFind))) {
 			dateFieldName.addAll(getDateFieldName(subField, fatherField + "." + field.getName(), classToFind));
 		} else {
-			if (field.getType().equals(classToFind)) {
+			if (Objects.equals(field.getType(), classToFind)) {
 				String fieldName = field.getName();
 				dateFieldName.add(fatherField +"."+ fieldName);
 				System.out.println("Date field = " + fatherField +"."+ fieldName);
