855794227b79b01911764ba17b513329e628ba4d
Sylvain Maullet <Sylvain.maullet@gmail.com>
2018-02-15 10:36:34 +0100

    Code cleaning

--- accessfieldofatype/PojoGenerator.java
+++ accessfieldofatype/PojoGenerator.java
@@ -3,8 +3,8 @@ private static ObjectMapper mapper = new ObjectMapper();
 
 	public static void main(String[] args) throws InstanceCreationException, IllegalAccessException, InstantiationException, InvocationTargetException {
 		Map<String, List<Object>> injectDataSet  = new HashMap<>();
-		PojoGenerator generator = new PojoGenerator(injectDataSet);
-		ElasticsearchMessage elasticsearchMessage = generator.generatePojoFed(ElasticsearchMessage.class);
-//		String json = mapper.writeValueAsString(elasticsearchMessage);
+		PojoFeeder feeder = new PojoFeeder(injectDataSet);
+		WhatEverPojo myPojoFed = feeder.generatePojoFed(WhatEverPojo.class);
+//		String json = mapper.writeValueAsString(myPojoFed);
 	}
   }
