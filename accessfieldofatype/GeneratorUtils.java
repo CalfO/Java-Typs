5773968bbc2a74c228aa1a11291ce978de612efe
Sylvain Maullet <Sylvain.maullet@gmail.com>
2018-02-15 10:34:53 +0100

    Create GeneratorUtils

---
+++ accessfieldofatype/GeneratorUtils.java
@@ -0,0 +1,27 @@
+public class GeneratorUtils {
+//TODO Fix package
+
+	public static Character[] generateCharArray(String luceneName) {
+		return new Character[]{'a', 'b'};
+	}
+
+	public static String generateStringValue(String luceneName) {
+		return "test";
+	}
+
+	public static Character generateCharValue(String luceneName) {
+		return 'a';
+	}
+
+	public static Object generateNumericValue(String luceneName) {
+		return 1;
+	}
+
+	public static Byte[] generateByteArray(String luceneName) {
+		return new Byte[0];
+	}
+
+	public static Byte generateByteValue(String luceneName) {
+		return null;
+	}
+}
